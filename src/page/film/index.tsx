a0bff339ce6c7c9f999c850de875fb7a6df7a9ec
blognn_287 <83860336+pikadragonelse@users.noreply.github.com>
2023-10-26 21:05:00 +0700

    feat: set up film

--- src/page/film/index.tsx
+++ src/page/film/index.tsx
@@ -1,6 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { Header } from "../../components/header";
+import { CompositeScreenProps, useNavigation } from "@react-navigation/native";
+import { Button } from "@rneui/base";
+import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { TabParamList } from "../../components/tab-navigator";
+import { RootStackParamList } from "../../../App";
+import { StackScreenProps } from "@react-navigation/stack";
 
-export const Film = () => {
-    return <View></View>;
+type FilmScreenProp = CompositeScreenProps<
+    BottomTabScreenProps<TabParamList>,
+    StackScreenProps<RootStackParamList>
+>;
+
+export const Film = ({ navigation, route }: FilmScreenProp) => {
+    return (
+        <View style={styles.filmContainer}>
+            <Header navigation={navigation} route={route} />
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    filmContainer: {
+        // backgroundColor: "#212121",
+        backgroundColor: "transparent",
+        paddingBottom: 10,
+    },
+});
